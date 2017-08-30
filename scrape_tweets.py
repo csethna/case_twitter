ad72637368bb78764212609fa827d941ab8639ee
Cyrus Sethna <csethna@gmail.com>
2017-08-30 06:40:34 -0500

    group hacking
    afternoon session

--- scrape_tweets.py
+++ scrape_tweets.py
@@ -1,8 +1,31 @@
 import tweepy
 from credentials import *
+import json
 
 def get_tweets():
     #Get the auth stuff in order
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
+
+    # This function pulls some recent tweets from @FAS_Outreach handle
+    tweets = api.user_timeline(screen_name = 'FAS_Outreach', include_rts= True, parser=tweepy.parsers.JSONParser())
+    return tweets
+
+#Does this work? Let's find out:
+test = get_tweets()
+print(test)
+
+# def text_finder():
+#     text = str(get_tweets())
+#     data = json.loads(text)
+#     return data
+#
+# test = text_finder()
+# print(test)
+
+    # trimmed_tweets = tweet_cache.cache_and_trim('tweet_ids.csv', tweets)
+#
+#     return trimmed_tweets
+#
+# print(trimmed_tweets)
